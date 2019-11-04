15ac87cc93f0054ae61f8af8a0944a591221fca4
cristian <cristian@socialandbeyond.com>
2019-11-03 21:44:09 -0600

    Delete and Hide unused views

--- src/index.js
+++ src/index.js
@@ -22,19 +22,17 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Admin from "layouts/Admin.js";
-import RTL from "layouts/RTL.js";
 
 import "assets/css/material-dashboard-react.css?v=1.8.0";
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+	<Router history={hist}>
+		<Switch>
+			<Route path="/admin" component={Admin} />
+			<Redirect from="/" to="/admin/dashboard" />
+		</Switch>
+	</Router>,
+	document.getElementById("root")
 );
--- src/routes.js
+++ src/routes.js
@@ -22,8 +22,6 @@ import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import BubbleChart from "@material-ui/icons/BubbleChart";
 import LocationOn from "@material-ui/icons/LocationOn";
 import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
-import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
@@ -32,83 +30,72 @@ import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
 import Maps from "views/Maps/Maps.js";
 import NotificationsPage from "views/Notifications/Notifications.js";
-import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
-// core components/views for RTL layout
-import RTLPage from "views/RTLPage/RTLPage.js";
 
 const dashboardRoutes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    rtlName: "لوحة القيادة",
-    icon: Dashboard,
-    component: DashboardPage,
-    layout: "/admin"
-  },
-  {
-    path: "/user",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
-    icon: Person,
-    component: UserProfile,
-    layout: "/admin"
-  },
-  {
-    path: "/table",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
-    icon: "content_paste",
-    component: TableList,
-    layout: "/admin"
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    rtlName: "طباعة",
-    icon: LibraryBooks,
-    component: Typography,
-    layout: "/admin"
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    rtlName: "الرموز",
-    icon: BubbleChart,
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    rtlName: "خرائط",
-    icon: LocationOn,
-    component: Maps,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    rtlName: "إخطارات",
-    icon: Notifications,
-    component: NotificationsPage,
-    layout: "/admin"
-  },
-  {
-    path: "/rtl-page",
-    name: "RTL Support",
-    rtlName: "پشتیبانی از راست به چپ",
-    icon: Language,
-    component: RTLPage,
-    layout: "/rtl"
-  },
-  {
-    path: "/upgrade-to-pro",
-    name: "Upgrade To PRO",
-    rtlName: "التطور للاحترافية",
-    icon: Unarchive,
-    component: UpgradeToPro,
-    layout: "/admin"
-  }
+	{
+		path: "/dashboard",
+		name: "Dashboard",
+		icon: Dashboard,
+		component: DashboardPage,
+		layout: "/admin"
+	}
 ];
 
-export default dashboardRoutes;
+const devRoutes = [
+	{
+		path: "/dashboard",
+		name: "Dashboard",
+		icon: Dashboard,
+		component: DashboardPage,
+		layout: "/admin"
+	},
+	{
+		path: "/user",
+		name: "User Profile",
+		icon: Person,
+		component: UserProfile,
+		layout: "/admin"
+	},
+	{
+		path: "/table",
+		name: "Table List",
+		icon: "content_paste",
+		component: TableList,
+		layout: "/admin"
+	},
+	{
+		path: "/typography",
+		name: "Typography",
+		icon: LibraryBooks,
+		component: Typography,
+		layout: "/admin"
+	},
+	{
+		path: "/icons",
+		name: "Icons",
+		icon: BubbleChart,
+		component: Icons,
+		layout: "/admin"
+	},
+	{
+		path: "/maps",
+		name: "Maps",
+		icon: LocationOn,
+		component: Maps,
+		layout: "/admin"
+	},
+	{
+		path: "/notifications",
+		name: "Notifications",
+		icon: Notifications,
+		component: NotificationsPage,
+		layout: "/admin"
+	}
+];
+
+const params = new URLSearchParams(window.location.search);
+const allTheme = params.get('allTheme');
+
+const routes = allTheme ? devRoutes : dashboardRoutes;
+
+export default routes;
